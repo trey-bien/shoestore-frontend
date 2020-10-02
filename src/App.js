b03e844e79eb14dbc3adb7fda68c0e1538da2818
Trey Dickerson <treydickerson@gmail.com>
2020-10-01 21:43:37 -0600

    rubric endpoints met

--- src/App.js
+++ src/App.js
@@ -2,25 +2,41 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shoes: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8000/api/shoe/")
+      .then((res) => res.json())
+      .then((data) => this.setState({shoes: data}));
+  }
+  
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.shoes.map((r) => (
+            <div>
+              <li>Size: {r.size}</li>
+              <li>Brand: {r.brand_name}</li>
+              <li>Manufacturer: {r.manufacturer}</li>
+              <li>Color: {r.color}</li>
+              <li>Material: {r.material}</li>
+              <li>Type: {r.shoe_type}</li>
+              <li>Fastener: {r.fasten_type}</li>
+              <br></br>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+
+  }
 }
 
 export default App;
